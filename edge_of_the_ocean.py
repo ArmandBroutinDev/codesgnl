b9e371d9c086fa580abd112b783ffe50fb5ac5a7
Armand B <71330618+ArmandBroutinDev@users.noreply.github.com>
2020-10-08 11:24:34 +0200

    Update edge_of_the_ocean.py
    fin (avec almostincreasingsequence copié d'ailleurs parce que ça m'a fatigué)

--- edge_of_the_ocean.py
+++ edge_of_the_ocean.py
@@ -21,3 +21,34 @@ def makeArrayConsecutive2(statues):
             res += (statues[i+1] - statues[i])-1
     return res
 
+def almostIncreasingSequence(sequence):
+    removed_one = False
+    prev_maxval = None
+    maxval = None
+    for s in sequence:
+        if not maxval or s > maxval:
+            prev_maxval = maxval
+            maxval = s
+        elif not prev_maxval or s > prev_maxval:
+            if removed_one:
+                return False
+            removed_one = True
+            maxval = s
+        else:
+            if removed_one:
+                return False
+            removed_one = True
+    return True
+
+def matrixElementsSum(matrix):
+    skipcol = [-1]*5
+    skipsize=0
+    total=0
+    for i in range(len(matrix)):
+         for j in range(len(matrix[0])):
+             if(j not in skipcol):
+                 if(matrix[i][j] == 0):
+                     skipcol[skipsize] = j
+                     skipsize+=1
+                 total += matrix[i][j]
+    return total
